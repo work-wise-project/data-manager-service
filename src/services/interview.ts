34eba966e2c3ee42284d092a6c66376951ee95c6
idoakstein1 <idoakstein1@gmail.com>
2025-04-25 13:14:40 +0300

    finished interview analysis

--- src/services/interview.ts
+++ src/services/interview.ts
@@ -5,4 +5,8 @@ export const getInterviewAnalysis = async (interviewId: string) =>
     await prisma.interview_analysis.findFirst({ where: { interview_id: interviewId } });
 
 export const createInterviewAnalysis = async (analysis: CreateInterviewAnalysisSchema) =>
-    await prisma.interview_analysis.create({ data: analysis });
+    await prisma.interview_analysis.upsert({
+        create: analysis,
+        update: analysis,
+        where: { interview_id: analysis.interview_id },
+    });
